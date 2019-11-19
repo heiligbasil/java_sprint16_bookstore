5692e14ac7a2f6ca60a42f64dd0dcf5a8fce0d2a
heiligbasil <basilief@yahoo.com>
2019-11-19 15:41:53 -0800

    Alternative CORS filter

--- src/main/java/com/lambdaschool/bookstore/config/SimpleCorsFilter.java
+++ src/main/java/com/lambdaschool/bookstore/config/SimpleCorsFilter.java
@@ -17,7 +17,7 @@ public class SimpleCorsFilter implements Filter {
     public SimpleCorsFilter() {
     }
 
-    @Override
+    /*@Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
@@ -28,6 +28,25 @@ public class SimpleCorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
 
+        if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }*/
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        // response.setHeader("Access-Control-Allow-Methods",
+        //                   "*");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type, access_token");
+        // response.setHeader("Access-Control-Allow-Headers",
+        //                   "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
         if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
